a91ad1abbe2ec78f8c074e81d30ffe2a33717d61
IrinaKimeisha <irinkakim777@gmail.com>
2022-12-10 01:47:25 +0300

    add EndPoints⛏

--- src/test/java/utils/EndPoints.java
+++ src/test/java/utils/EndPoints.java
@@ -3,8 +3,9 @@ package utils;
 public class EndPoints {
     public static final String GET_PROJECTS = "index.php?/api/v2/get_projects";
     public static final String GET_PROJECT = "index.php?/api/v2/get_project/{project_id}";
-
     public static final String ADD_PROJECT = "index.php?/api/v2/add_project";
 
+    public static final String GET_SUITES = "index.php?/api/v2/get_suites/{project_id}";
+    public static final String ADD_SUITES = "index.php?/api/v2/add_suite/{project_id}";
 
 }
